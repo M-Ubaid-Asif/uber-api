fc79cd4fcac9329fbef1468f49529d20fb606335
mohammad ubaid <ubaid6664@gmail.com>
2022-03-30 10:45:51 +0530

    updated user controller

--- components/user/userController.js
+++ components/user/userController.js
@@ -1,67 +1,65 @@
-import logger from "../../config/logger"
-import User from "./userModel"
+import logger from "../../config/logger";
+import User from "./userModel";
+import { create, findOne } from "../../helpers/common";
 
+export const registerUser = async (req, res, next) => {
+  try {
+    logger.info("inside User Controller Register user");
+    const { name, email, password, confirmPassword, contactNo } = req.body;
 
-export const registerUser = async (req,res,next)=>{
-    try {
-        logger.info("inside User Controller Register user")
-        const {name,email,password,confirmPassword,contactNo} = req.body
-        
-        const isExist = await User.findOne({email})
-        
-        if(isExist){
-            return res.status(409).json({
-                message:"email is already registered"
-            })
-        }
-    
-        const user = await User.create({
-            name,
-            email,
-            password,
-            confirmPassword,
-            contactNo
-        })
-    
-        res.status(201).json({
-            message:"registration success",
-            data:user
-    
-        })
-    } catch (error) {
-        next(new Error(error));
+    const isExist = await User.findOne({ email });
+
+    if (isExist) {
+      return res.status(409).json({
+        message: "email is already registered",
+      });
     }
-}
-
-export const loginUser = async(req,res,next)=>{
-    try {
-        logger.info("inside user controller login user")
-        const {email,password} = req.body
-
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(401).json({
-                message:"Invalid credentials"
-            })
-        }
-        
-        
-        const isValid =await user.comparePassword(password)
-
-        if(!isValid){
-            return res.status(401).json({
-                message:"invalid credentials"
-            })
-        }
-        
-        
-        return res.status(200).json({
-            message:"login success"
+    const data = {
+      name,
+      email,
+      password,
+      confirmPassword,
+      contactNo,
+    };
+    const user = await create(User, data);
+    return user
+      ? res.status(201).json({
+          message: "registration success",
+          data: user,
         })
+      : res.status(400).json({
+          message: "registration failed",
+        });
+  } catch (error) {
+    next(new Error(error));
+  }
+};
 
+// login user
+export const loginUser = async (req, res, next) => {
+  try {
+    logger.info("inside user controller login user");
+    const { email, password } = req.body;
 
-
-    } catch (error) {
-        next(new Error(error));
+    const user = await findOne(User, { email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
     }
-}
+
+    const isValid = await user.comparePassword(password);
+
+    if (!isValid) {
+      return res.status(401).json({
+        message: "invalid credentials",
+      });
+    }
+
+    return res.status(200).json({
+      message: "login success",
+    });
+  } catch (error) {
+    next(new Error(error));
+  }
+};
