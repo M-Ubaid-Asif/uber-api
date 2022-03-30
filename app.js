cd2894f9c2a98818dbf7bece31d5fabbde39bcce
mohammad ubaid <ubaid6664@gmail.com>
2022-03-30 11:27:08 +0530

    added error handler middleware

--- app.js
+++ app.js
@@ -22,11 +22,30 @@ app.get("/", (req, res) => {
     message: "success",
   });
 });
-app.use("/api", router);
 
+// all routes
+app.use("/api/v1", router);
+
+// db connection
 db();
 
+// not found error
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  return res.status(404).json({
+    message: error.message,
+  });
+});
+
+// db error
 app.use(mongoseErrors);
+
+// development and production error
+if (config.node_env === "development") {
+  app.use(developmentErrors);
+} else {
+  app.use(productionErrors);
+}
 
 app.listen(config.server.port, () => {
   logger.info(`server is running on ${config.server.port}`);
