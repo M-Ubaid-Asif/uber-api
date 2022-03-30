47fbe4f30a7c072b82bf978eec46c79e4bcd6c03
mohammad ubaid <ubaid6664@gmail.com>
2022-03-30 11:23:24 +0530

    added error handler middleware

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
 import logger from "./config/logger.js";
-import express from "express";
-import cors from 'cors';
+import express, { json, urlencoded } from "express";
+import cors from "cors";
 import morgan from "morgan";
 import db from "./connections/dbConnection.js";
 import config from "./config/config.js";
 import cookieParser from "cookie-parser";
 import router from "./components/index.js";
-
+import { mongoseErrors } from "./helpers/errorHandler.js";
 
 const app = express();
 
-
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 app.use(cors());
 app.use(cookieParser());
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        message:"success"
-    })
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "success",
+  });
 });
-app.use('/api',router)
-
-
-
+app.use("/api", router);
 
 db();
 
-app.listen(config.server.port,()=>{
-    logger.info(`server is running on ${config.server.port}`)
-})
+app.use(mongoseErrors);
 
-export default app
+app.listen(config.server.port, () => {
+  logger.info(`server is running on ${config.server.port}`);
+});
 
-
-
-
-
-
+export default app;
