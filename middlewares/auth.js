fcede267f801438b4313d831a371afa56b791318
mohammad ubaid <ubaid6664@gmail.com>
2022-03-31 17:38:22 +0530

    added admin and driver auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,30 +1,96 @@
+import Admin from "../components/admin/adminModel";
+import Driver from "../components/driver/driverModel";
 import User from "../components/user/userModel";
 import { findOne } from "../helpers/common";
 import { verifyJwt } from "../utils/jwt";
 
 // auth for user
 export const userAuth = async function (req, res, next) {
-  const cookie = req.headers.cookie;
-  if (!cookie) {
-    res.status(401).json({
-      message: "you are not authenticated user",
-    });
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) {
+      res.status(401).json({
+        message: "you are not authenticated user",
+      });
+    }
+    const token = cookie.split("=")[1];
+    console.log(token);
+    const decode = verifyJwt(token);
+    console.log(decode);
+
+    const user = await findOne(User, { _id: decode.id });
+    console.log(user);
+    if (!user) {
+      return res.status(401).json({
+        message: "access denied",
+      });
+    }
+
+    req.user = decode;
+
+    next();
+  } catch (error) {
+    next(new Error(error));
   }
-  const token = cookie.split("=")[1];
-  console.log(token);
-  const decode = verifyJwt(token);
-  console.log(decode);
+};
 
-  const user = await findOne(User, { _id: decode.id });
-  console.log(user);
-  if (!user) {
-    return res.status(401).json({
-      message: "you are not authenticated user",
-    });
+// auth for admin
+export const adminAuth = async function (req, res, next) {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) {
+      res.status(401).json({
+        message:
+          "you are not authenticated! this route can be access by admin only",
+      });
+    }
+    const token = cookie.split("=")[1];
+    console.log(token);
+    const decode = verifyJwt(token);
+    console.log(decode);
+
+    const admin = await findOne(Admin, { _id: decode.id });
+    console.log(admin);
+    if (!admin) {
+      return res.status(401).json({
+        message: "access denied! this route can be access by admin only",
+      });
+    }
+
+    req.admin = decode;
+
+    next();
+  } catch (error) {
+    next(new Error(error));
   }
+};
 
-  req.user = decode;
-  // const token
+// auth for driver
+export const driverAuth = async function (req, res, next) {
+  try {
+    const cookie = req.headers.cookie;
+    if (!cookie) {
+      res.status(401).json({
+        message: "you are not authenticated!",
+      });
+    }
+    const token = cookie.split("=")[1];
+    console.log(token);
+    const decode = verifyJwt(token);
+    console.log(decode);
 
-  next();
+    const driver = await findOne(Driver, { _id: decode.id });
+    console.log(driver);
+    if (!driver) {
+      return res.status(401).json({
+        message: "Access denied",
+      });
+    }
+
+    req.driver = decode;
+
+    next();
+  } catch (error) {
+    next(new Error(error));
+  }
 };
