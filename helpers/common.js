73bc7f07e101eddcce9e7904ed65eae2a704d2ce
mohammad ubaid <ubaid6664@gmail.com>
2022-03-31 10:35:07 +0530

    updated helpers

--- helpers/common.js
+++ helpers/common.js
@@ -11,7 +11,28 @@ export const create = async (model, data) => {
 // find one factory function
 export const findOne = async (model, data) => {
   try {
+    data.isDeleted = false;
     const doc = await model.findOne(data);
+    return doc ? doc : false;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const find = async (model) => {
+  try {
+    data.isDeleted = false;
+    const doc = await model.find();
+    return doc ? doc : false;
+  } catch (error) {
+    return false;
+  }
+};
+
+// find all cabs
+export const findCabs = async (model, filter) => {
+  try {
+    const doc = await model.find(filter);
     return doc ? doc : false;
   } catch (error) {
     return false;
