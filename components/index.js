4b2bec8811726808fc48dbf1a3d16199257e4adb
mohammad ubaid <ubaid6664@gmail.com>
2022-03-31 15:06:21 +0530

    added booking routes

--- components/index.js
+++ components/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import adminRouter from "./admin/adminRoute";
+import bookingRouter from "./bookings/bookingRoute";
+import cabRouter from "./cab/cabRoutes";
+import driverRouter from "./driver/driverRoute";
 import userRouter from "./user/userRoute";
-adminRouter
+adminRouter;
 const router = express.Router();
 
+router.use("/user", userRouter);
+router.use("/admin", adminRouter);
+router.use("/booking", bookingRouter);
+router.use("/driver", driverRouter);
+router.use("/cab", cabRouter);
 
-router.use('/user',userRouter)
-router.use('/admin',adminRouter)
-
-export default router
+export default router;
