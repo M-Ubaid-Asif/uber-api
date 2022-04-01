35bad61a246e39bc97917ed9b15a5767fecb2cd3
mohammad ubaid <ubaid6664@gmail.com>
2022-04-01 12:09:42 +0530

    updated common.js

--- helpers/common.js
+++ helpers/common.js
@@ -1,6 +1,4 @@
-import Booking from "../components/bookings/bookingModel";
-
-// create factory function
+// create doc
 export const create = async (model, data) => {
   try {
     const doc = await model.create(data);
@@ -10,7 +8,7 @@ export const create = async (model, data) => {
   }
 };
 
-// find one factory function
+// find one doc
 export const findOne = async (model, data) => {
   try {
     data.isDeleted = false;
@@ -21,45 +19,26 @@ export const findOne = async (model, data) => {
   }
 };
 
+// find all doc
 export const find = async (model) => {
   try {
-    data.isDeleted = false;
-    const doc = await model.find();
+    const doc = await model.find({ isDeleted: false });
     return doc ? doc : false;
   } catch (error) {
     return false;
   }
 };
 
-// find all cabs
-export const findCab = async (model, filter) => {
-  try {
-    console.log("hahhahah");
-    console.log(filter);
-    const doc = await model.findOne(filter);
-    console.log(doc);
-    // console.log("----->");
-    return doc ? doc : false;
-  } catch (error) {
-    return false;
-  }
-};
-
-// deleteone
-
+// delete one doc
 export const deleteOne = async (model, data) => {
   try {
-    data.isDeleted = false;
+    console.log("deleteone");
     console.log(data);
-    const doc = await model.find(data);
-    console.log(doc);
-    if (!doc) {
-      return false;
-    }
-    doc.isDeleted = true;
-    console.log(doc);
-    await doc.save();
-    return doc;
+    const doc = await model.findOneAndUpdate(data, {
+      isDeleted: true,
+    });
+    // console.log("got data", doc);
+    return doc ? doc : false;
   } catch (error) {
     new Error(error);
   }
